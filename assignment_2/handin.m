fc6ada2ad410853b858ad907bd4721c55d013bb9
voldemorten <mortendlaursen@gmail.com>
2019-05-14 18:33:34 +0900

    LDA demo

--- assignment_2/handin.m
+++ assignment_2/handin.m
@@ -8,7 +8,8 @@ m_1 = [3;1];
 m_2 = [1;3];
 sig_1 = [1 2; 2 5];
 sig_2 = sig_1;
-n = 10;
+n = 100;
+c = 2;
 
 % debug
 show_plots = 0;
@@ -18,13 +19,13 @@ R1 = mvnrnd(m_1,sig_1,n);
 R2 = mvnrnd(m_2,sig_2,n);
 
 if show_plots
-	figure; plot(R1(:,1),R1(:,2),'r+'); hold on; plot(R2(:,1),R2(:,2),'bo'); legend("R1","R2");
-	axis([-2 8 -2 8], "square");
+	figure; plot(R1(:,1),R1(:,2),'r+'); hold on; plot(R2(:,1),R2(:,2),'bo');
+	axis([-10 10 -10 10], "square");
 	printf("Showing original plot. Press any key to draw P1 and P2");
 	input("","s");
 end
 
-% Exercise 2 (PCA)
+% Exercise 2 - PCA)
 % Step 1: Merge data;
 R = [R1;R2];
 % step 2: Calculate covariance matrix
@@ -34,27 +35,50 @@ cov_R = cov(R);
 % step 4: Eigenvalues are not ordered. Sort them
 [eig_val, i] = sort(diag(eig_val), 'descend');
 eig_vec = eig_vec(:,i);
+eig_vec_lda = eig_vec;
 
 %scale to scale points such that our P-lines get longer.
 scale = 10;
 
 if show_plots
 	% Draw P1, P2
-	line([-eig_vec(1,1) * scale eig_vec(1,1) * scale],[-eig_vec(2,1) * scale eig_vec(2,1) * scale], "linestyle", "-", "color", "b");
+	line([-eig_vec(1,1) * scale eig_vec(1,1) * scale],[-eig_vec(2,1) * scale eig_vec(2,1) * scale], "linestyle", "-", "color", "r");
 	line([-eig_vec(1,2) * scale eig_vec(1,2) * scale],[-eig_vec(2,2) * scale eig_vec(2,2) * scale], "linestyle", "-", "color", "b");
+	legend("R1","R2","P1","P2");
 
-	printf("Press any key to move data by substracting mean");
+	printf("Press any key to move data by substracting mean\n");
 	input("","s");
 
 	% Move data by mean
-	R = R-mean(R);
+	R_new = R-mean(R);
 	figure; hold on;
-	plot(R(1:n,1),R(1:n,2),'r+'); plot(R(n+1:n*2,1),R(n+1:n*2,2),'bo');
-	legend("P1","P2","R1","R2"); axis("square");
-	xlim([-5 5]);
-	ylim([-5 5]);
-	axis("square");
+	plot(R_new(1:n,1),R_new(1:n,2),'r+'); plot(R_new(n+1:n*2,1),R_new(n+1:n*2,2),'bo');
+	axis([-10 10 -10 10], "square");
 
-	line([-eig_vec(1,1) * scale eig_vec(1,1) * scale],[-eig_vec(2,1) * scale eig_vec(2,1) * scale], "linestyle", "-", "color", "b");
+	line([-eig_vec(1,1) * scale eig_vec(1,1) * scale],[-eig_vec(2,1) * scale eig_vec(2,1) * scale], "linestyle", "-", "color", "r");
 	line([-eig_vec(1,2) * scale eig_vec(1,2) * scale],[-eig_vec(2,2) * scale eig_vec(2,2) * scale], "linestyle", "-", "color", "b");
+
+	legend("R1","R2","P1","P2");
 endif
+
+% Exercise 3 - LDA
+% TODO: UNDERSTAND AND REWRITE!
+printf("Press any key to continue with LDA");
+input("","s");
+mu_total = mean(R)
+mu = [ mean(R1); mean(R2) ]
+Sw = (R - mu(c,:))'*(R - mu(c,:))
+Sb = (ones(c,1) * mu_total - mu)' * (ones(c,1) * mu_total - mu)
+[eig_vec, eig_val] = eig(Sw\Sb)
+[eig_val, i] = sort(diag(eig_val), 'descend');
+eig_vec = eig_vec(:,i);
+R_new = R-mean(R);
+
+if show_plots
+	figure; hold on;
+	plot(R_new(1:n,1),R_new(1:n,2),'r+'); plot(R_new(n+1:n*2,1),R_new(n+1:n*2,2),'bo');
+	axis([-10 10 -10 10], "square");
+
+	line([-eig_vec(1,1) * scale eig_vec(1,1) * scale],[-eig_vec(2,1) * scale eig_vec(2,1) * scale], "linestyle", "-", "color", "r");
+	legend("R1","R2","P1");
+end
