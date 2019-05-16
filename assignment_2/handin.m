85e569c96b4e0844ae8328df1ba7e06a8cadefc0
voldemorten <mortendlaursen@gmail.com>
2019-05-16 18:01:11 +0900

    something's working '

--- assignment_2/handin.m
+++ assignment_2/handin.m
@@ -1,5 +1,5 @@
 % load packages
-pkg load statistics; %pkg load
+pkg load statistics;
 
 clear; close all;
 
@@ -8,7 +8,7 @@ m_1 = [3;1];
 m_2 = [1;3];
 sig_1 = [1 2; 2 5];
 sig_2 = sig_1;
-n = 100;
+n = 1000;
 c = 2;
 
 % debug
@@ -17,6 +17,11 @@ show_plots = 1;
 % Exercise 1 - Initialize data
 R1 = mvnrnd(m_1,sig_1,n);
 R2 = mvnrnd(m_2,sig_2,n);
+
+%Demo
+% R1 = [2 3;3 4;4 5;5 6;5 7];
+% R2 = [2 1;3 2;4 2;4 3;6 4;7 6];
+% n = 5;
 
 if show_plots
 	figure; plot(R1(:,1),R1(:,2),'r+'); hold on; plot(R2(:,1),R2(:,2),'bo');
@@ -59,6 +64,37 @@ if show_plots
 	line([-eig_vec(1,2) * scale eig_vec(1,2) * scale],[-eig_vec(2,2) * scale eig_vec(2,2) * scale], "linestyle", "-", "color", "b");
 
 	legend("R1","R2","P1","P2");
+
+	z = R_new*eig_vec(:,2)
+	% and reconstruct it
+	p = z*eig_vec(:,2)'
+	p = bsxfun(@plus, p, mean(R))
+	R_new = p;
+	figure; hold on;
+	line([-eig_vec(1,1) * scale eig_vec(1,1) * scale],[-eig_vec(2,1) * scale eig_vec(2,1) * scale], "linestyle", "-", "color", "r");
+	line([-eig_vec(1,2) * scale eig_vec(1,2) * scale],[-eig_vec(2,2) * scale eig_vec(2,2) * scale], "linestyle", "-", "color", "b");
+
+	p1 = plot(R_new(1:n,1),R_new(1:n,2),"ro", "markersize", 10, "linewidth", 3);
+	p2 = plot(R_new(n+1:n*2,1), R_new(n+1:n*2,2),"go", "markersize", 10, "linewidth", 3);
+
+	legend("R1","R2","P1","P2");
+
+	% By courtesy of https://stackoverflow.com/questions/14530944/octave-creating-two-histograms-with-color-blending, to work around octaves missing facealpha / transparency function.
+	figure;
+	hold on;
+	[y1, x1] = hist(R_new(1:n,:)*eig_vec(1,:)',20);
+	[y2, x2] = hist(R_new(n+1:n*2,:)*eig_vec(1,:)',20);
+	[ys1 xs1] = stairs(y1, x1);
+	[ys2 xs2] = stairs(y2, x2);
+	xs1 = [xs1(1); xs1; xs1(end)];  xs2 = [xs2(1); xs2; xs2(end)];
+	ys1 = [0; ys1; 0];  ys2 = [0; ys2; 0];
+	clf
+	hold on;
+	h1=fill(xs1,ys1,"red");
+	h2=fill(xs2,ys2,"green");
+	set(h1,'facealpha',0.5);
+	set(h2,'facealpha',0.5);
+	hold off;
 endif
 
 % Exercise 3 - LDA
